9b9e021c494efeca9eebd2f5e9ac3543f6463317
Miralem.Ba <miralembanovi@gmail.com>
2024-05-23 10:32:08 +0200

    I have added timestamp VARCHAR(255) NOT NULL and comments in the database.js.

--- server/database.js
+++ server/database.js
@@ -1,21 +1,13 @@
+// Globale Variable, die den Verbindungs-Pool für die MariaDB-Verbindungen speichert
 let pool = null;
 
-/**
- * Initializes the MariaDB connection pool.
- * The connection pool is used to execute SQL queries.
- * The connection pool is created with the following parameters:
- * - database: The name of the database to connect to. (process.env.DB_NAME)
- * - host: The host of the database. (process.env.DB_HOST)
- * - user: The user to connect to the database. (process.env.DB_USER)
- * - password: The password to connect to the database. (process.env.DB_PASSWORD)
- * - connectionLimit: The maximum number of connections in the pool. (5)
- * @example
- * initializeMariaDB();
- * @returns {void}
- * @see {@link https://mariadb.com/kb/en/mariadb-connector-nodejs-pooling/}
- */
+// Funktion zur Initialisierung der MariaDB-Verbindung
 const initializeMariaDB = () => {
+
+  // Importieren des mariadb-Moduls
   const mariadb = require("mariadb");
+
+  // Erstellen eines Verbindungs-Pools mit den Konfigurationsparametern
   pool = mariadb.createPool({
     database: process.env.DB_NAME || "mychat",
     host: process.env.DB_HOST || "localhost",
@@ -25,49 +17,48 @@ const initializeMariaDB = () => {
   });
 };
 
-/**
- * Allows the execution of SQL queries.
- * @example
- * // Insert statement with a parameter. Can be multiple in an array format like ["Patrick", 1]
- * executeSQL("INSERT INTO users value (?)", ["Patrick"]);
- * @example
- * // Select statement without parameters.
- * executeSQL("SELECT * FROM users;");
- * @returns {Array} Returns the result of the query.
- */
-const executeSQL = async (query, params) => {
+// Funktion zur Ausführung von SQL-Abfragen
+const executeSQL = async (query) => {
   let conn;
   try {
+    // Abrufen einer Verbindung aus dem Pool
     conn = await pool.getConnection();
-    const res = await conn.query(query, params);
+    // Ausführen der SQL-Abfrage
+    const res = await conn.query(query);
+    // Zurückgeben des Ergebnisses der Abfrage
     return res;
   } catch (err) {
+    // Fehlerbehandlung bei der Ausführung der Abfrage
     console.log(err);
   } finally {
+    // Freigeben der Verbindung zurück in den Pool
     if (conn) conn.release();
   }
 };
 
-/**
- * Initializes the database schema.
- * Creates the tables if they do not exist.
- * Useful for the first time setup.
- */
+// Funktion zur Initialisierung des Datenbankschemas
 const initializeDBSchema = async () => {
+  // SQL-Abfrage zum Erstellen der "users"-Tabelle, falls sie noch nicht existiert
   const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
   );`;
+ // Ausführen der SQL-Abfrage zum Erstellen der "users"-Tabelle
   await executeSQL(userTableQuery);
+
+  // SQL-Abfrage zum Erstellen der "messages"-Tabelle, falls sie noch nicht existiert
   const messageTableQuery = `CREATE TABLE IF NOT EXISTS messages (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     message VARCHAR(255) NOT NULL,
+    timestamp VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
   );`;
+  // Ausführen der SQL-Abfrage zum Erstellen der "messages"-Tabelle
   await executeSQL(messageTableQuery);
 };
 
+// Exportieren der Funktionen zur Nutzung in anderen Modulen
 module.exports = { executeSQL, initializeMariaDB, initializeDBSchema };
