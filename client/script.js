3902ee91dd340c562bae2e6c6aed24b09b1f344b
Miralem.Ba <miralembanovi@gmail.com>
2024-05-25 12:00:02 +0200

    Changes made

--- client/script.js
+++ client/script.js
@@ -3,24 +3,22 @@ const socket = new WebSocket("ws://localhost:3000");
 
 // Event-Listener für den Verbindungsaufbau
 socket.addEventListener("open", (event) => {
-    console.log("WebSocket connected!222222222222222");
+    console.log("WebSocket connected!");
     socket.send(JSON.stringify("Hello, server!")); // Nachricht an den Server senden, sobald die Verbindung hergestellt ist
-  });
+});
 
 // Event-Listener für das Schließen der Verbindung
 socket.addEventListener("close", (event) => {
-  console.log("WebSocket closed.");
+    console.log("WebSocket closed.");
 });
 
 // Event-Listener für WebSocket-Fehler
 socket.addEventListener("error", (event) => {
-  console.error("WebSocket error:", event);
+    console.error("WebSocket error:", event);
 });
 
 // Event-Listener für eingehende Nachrichten vom Server
 socket.addEventListener("message", (event) => {
-
-    // Die empfangenen Daten werden geparst
     const data = JSON.parse(event.data);
     const messagedatasWsOutput = data[0].message;
     const messageDatas = JSON.parse(messagedatasWsOutput)
@@ -30,139 +28,116 @@ socket.addEventListener("message", (event) => {
     const userBox = document.getElementById('users')
     const userdatadatasWsOutput = data[0].users;
     
-    // Schleife durch die Nachrichten und Hinzufügen zum Chat-Fenster
     for (let i = 0; i < messages.length; i++) {
+        const message = messages[i].message;
+        const name = messages[i].name;
+        const timestamp = messages[i].timestamp;
     
-        const message = (messages[i].message)
-        const name = (messages[i].name)
-        const timestamp = (messages[i].timestamp)
-    
-        // HTML-Struktur für jede Nachricht im Chat-Fenster
         const chatBoxElement = 
-        `<div id="chatbox" class="w-5/12 h-fit bg-indigo-100 rounded-lg">
+        `<div class="w-full bg-indigo-100 rounded-lg p-2 mb-2">
             <span class="text-xl font-bold">${name}</span> 
             <div class="border-t border-gray-500 my-1"></div> 
-            <p class="">${message}</p>
+            <p>${message}</p>
             <div class="border-t border-gray-500 my-1"></div> 
             <span class="text-xs text-left underline-offset-1">${timestamp}</span>
         </div>`;
 
-        // Nachricht zum Chat-Fenster hinzufügen
         chatBox.innerHTML += chatBoxElement;
     }
 
-    // Schleife durch die Benutzer und Hinzufügen zur Benutzerliste
     let usersInput = '';
     for (let i = 0; i < userdatadatasWsOutput.length; i++) {
-      const username = userdatadatasWsOutput[i].username;
+        const username = userdatadatasWsOutput[i].username;
 
-      // HTML-Struktur für jeden Benutzer in der Benutzerliste
-      const userElement = 
-        `<div class="user-item rounded-lg">
-          <span class="text-lg font-bold">${username}</span>
+        const userElement = 
+        `<div class="user-item bg-gray-300 p-2 mb-1">
+            <span class="text-lg font-bold">${username}</span>
         </div>`;
 
-      // Benutzer zur Benutzerliste hinzufügen
-      usersInput = `${usersInput}${userElement}`;
-      userBox.innerHTML = usersInput
+        usersInput = `${usersInput}${userElement}`;
+        userBox.innerHTML = usersInput;
     }
     
-    // Scroll das Chat-Fenster nach unten
     chatBox.scrollTop = chatBox.scrollHeight;
-})
+});
 
 // Funktion zum Auswählen eines Benutzers und Senden der Benutzerdaten über WebSocket
 const selectUser = async () => {
+    const userInput = document.getElementById('user-input');
+    const username = userInput.value;
 
-  // Zugriff auf das Eingabefeld für den Benutzernamen im DOM
-  const userInput = document.getElementById('user-input');
+    const userData = {
+        username: username
+    };
 
-  // Abrufen des Benutzernamens aus dem Eingabefeld
-  const username = userInput.value;
+    const wsUser = JSON.stringify([
+        {
+            type: "sendUserData",
+            message: JSON.stringify(userData)
+        }
+    ]);
 
-  // Erstellen eines Objekts mit den Benutzerdaten
-  const userData = {
-      username: username
-  };
-
-  // Erstellen einer Nachricht im JSON-Format, die die Benutzerdaten enthält
-  const wsUser = JSON.stringify([
-      {
-        type: "sendUserData", // Der Typ der Nachricht, der angibt, dass Benutzerdaten gesendet werden
-        message: JSON.stringify(userData) // Die Benutzerdaten selbst, als JSON-String
-      }
-    ])
-
-    // Senden der Nachricht über die WebSocket-Verbindung
-    socket.send(wsUser) 
+    socket.send(wsUser);
 };
 
-//Funktion zum Erstellen einer neuen Chat-Nachricht und Senden dieser Nachricht über WebSocket
+// Funktion zum Erstellen einer neuen Chat-Nachricht und Senden dieser Nachricht über WebSocket
 const newChatBox = async () => {
+    const chatBox = document.getElementById('chatwindow');
+    const msgInput = document.getElementById('message-input');
+    const userInput = document.getElementById('user-input');
+    const sendButton = document.getElementById('sendButton');
 
-  // Zugriff auf das Chat-Fenster und die Eingabefelder für Nachricht und Benutzername im DOM
-  const chatBox = document.getElementById('chatwindow');
-  const msgInput = document.getElementById('message-input');
-  const userInput = document.getElementById('user-input');
+    // Button deaktivieren
+    sendButton.disabled = true;
 
-  // Abrufen des Benutzernamens und der Nachricht aus den Eingabefeldern
-  const username = userInput.value;
-  const message = msgInput.value;
+    const username = userInput.value;
+    const message = msgInput.value;
 
-  // Erstellen eines Zeitstempels für die Nachricht
-  const timeStamp = new Date().toLocaleDateString("de-CH", {
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric",
+    const timeStamp = new Date().toLocaleDateString("de-CH", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
     });
 
-  // Erstellen eines Objekts mit den Chat-Daten
-  const chatData = {
-      username: username,
-      message: message,
-      timeStamp: timeStamp 
-  };
+    const chatData = {
+        username: username,
+        message: message,
+        timeStamp: timeStamp 
+    };
 
-  // HTML-Struktur für die neue Nachricht im Chat-Fenster
-  const chatBoxElement = 
-      `<div id="chatboxUser" class="w-5/12 h-fit bg-indigo-100 rounded-lg outline">
-          <span class="text-xl font-bold">${username}</span> 
-          <div class="border-t border-gray-500 my-1"></div> 
-          <p class="">${message}</p>
-          <div class="border-t border-gray-500 my-1"></div> 
-          <span class="text-xs text-left underline-offset-1">${timeStamp}</span>
-      </div>`;
+    const chatBoxElement = 
+        `<div class="w-full bg-indigo-100 rounded-lg p-2 mb-2">
+            <span class="text-xl font-bold">${username}</span> 
+            <div class="border-t border-gray-500 my-1"></div> 
+            <p>${message}</p>
+            <div class="border-t border-gray-500 my-1"></div> 
+            <span class="text-xs text-left underline-offset-1">${timeStamp}</span>
+        </div>`;
 
-  // Hinzufügen der neuen Nachricht zum Chat-Fenster
-  chatBox.innerHTML += chatBoxElement;
+    chatBox.innerHTML += chatBoxElement;
+    chatBox.scrollTop = chatBox.scrollHeight;
 
-  // Scroll das Chat-Fenster nach unten, um die neueste Nachricht anzuzeigen
-  chatBox.scrollTop = chatBox.scrollHeight;
+    const wsMessage = JSON.stringify([
+        {
+            type: "sendChatData",
+            message: JSON.stringify(chatData)
+        }
+    ]);
 
-  
-  // Erstellen der WebSocket-Nachricht im JSON-Format, die die Chat-Daten enthält
-  const wsMessage = JSON.stringify([
-    {
-      type: "sendChatData", // Der Typ der Nachricht, der angibt, dass Chat-Daten gesendet werden
-      message: JSON.stringify(chatData) // Die Chat-Daten selbst, als JSON-String
-    }
-  ])
+    socket.send(wsMessage);
 
-  // Senden der Nachricht über die WebSocket-Verbindung
-  socket.send(wsMessage)    
+    // Button nach dem Senden wieder aktivieren
+    sendButton.disabled = false;
 };
-
 
 // Event-Listener für das Laden des Dokuments
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("1111111")
+    console.log("Document loaded");
 
-    // Event-Listener für den "Senden"-Button
     document.getElementById('sendButton').addEventListener('click', function () {
         newChatBox();
     });
 
-    // Event-Listener für den "Benutzer auswählen"-Button
     document.getElementById('selectUser').addEventListener('click', function () {
         selectUser();
     });
